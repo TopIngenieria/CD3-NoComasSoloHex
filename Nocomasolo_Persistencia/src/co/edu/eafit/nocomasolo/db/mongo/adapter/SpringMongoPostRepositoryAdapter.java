eca60537fac9762d5b1de52dc3f78b5c552f7b0b
daniela0106 <daniela.barrientos.grisales@gmail.com>
2014-11-10 13:01:52 -0500

    Cambios en la capa de persistencia.

--- Nocomasolo_Persistencia/src/co/edu/eafit/nocomasolo/db/mongo/adapter/SpringMongoPostRepositoryAdapter.java
+++
@@ -1,9 +0,0 @@
-package co.edu.eafit.nocomasolo.db.mongo.adapter;
-
-public class SpringMongoPostRepositoryAdapter {
-
-	public SpringMongoPostRepositoryAdapter() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
